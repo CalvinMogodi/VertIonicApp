d776830ecc1e07902cd9fe78e6d44e0e3a926358
CalvinMogodi <Calvin.Mogodi@eoh.co.za>
2017-08-30 18:47:16 +0200

    xfgdfg

--- src/pages/postAnAdvert/postAnAdvert.ts
+++ src/pages/postAnAdvert/postAnAdvert.ts
@@ -1,7 +1,7 @@
-import {Component } from '@angular/core';
+import {Component ,ViewChild} from '@angular/core';
 import {AngularFire, FirebaseListObservable} from 'angularfire2';
-import { FormBuilder, FormGroup, Validators, FormControl } from '@angular/forms';
-import { NavController, ActionSheetController, ToastController, Platform, LoadingController, Loading } from 'ionic-angular';
+import { FormBuilder, FormGroup, Validators, FormControl, AbstractControl } from '@angular/forms';
+import { NavController, ActionSheetController, ToastController, Platform, LoadingController, Loading ,AlertController} from 'ionic-angular';
 
 import { File } from '@ionic-native/file';
 import { Transfer, TransferObject } from '@ionic-native/transfer';
@@ -14,8 +14,188 @@ declare var cordova: any;
   selector: 'page-postAnAdvert',
   templateUrl: 'postAnAdvert.html'
 })
+
 export class PostAnAdvertPage {
-  lastImage: string = null;
+   pages: Array<{title: string, component: any, icon: string}>;
+today = new Date();
+startTime = this.today.getHours()+":"+this.today.getMinutes();
+public advert = {
+  name: undefined,
+      category: undefined,
+      dateStart: new Date().toISOString(), 
+      timeStart: this.startTime,
+      dateEnd:  new Date().toISOString(), 
+      timeEnd: '12:59', 
+      mobileNumber: undefined, 
+      emailAddress: undefined, 
+      location: undefined, 
+      postAsaBusiness: false, 
+      businessName: '', 
+      businessRegistrationNumber: '', 
+      businessWebsite: '', 
+      isApproved: false,
+      imageRef: undefined,
+}
+  @ViewChild('signupSlider') signupSlider: any;
+    searchQuery: string = '';
+    items: string[];
+    slideOneForm: FormGroup;
+    slideTwoForm: FormGroup;
+    slideThreeForm: FormGroup;
+    slideFourForm: FormGroup;
+    businessName: AbstractControl;
+    businessWebsite: AbstractControl;
+    businessRegistrationNumber: AbstractControl;
+    submitAttempt: boolean = false;
+   
+ 
+  constructor(public navCtrl: NavController,public alertCtrl: AlertController, public formBuilder: FormBuilder,  af: AngularFire, private camera: Camera, private transfer: Transfer, private file: File, private filePath: FilePath, public actionSheetCtrl: ActionSheetController, public toastCtrl: ToastController, public platform: Platform, public loadingCtrl: LoadingController) {
+    
+    this.slideOneForm = formBuilder.group({
+        advertName: ['', Validators.compose([Validators.minLength(2), Validators.pattern('[a-zA-Z ]*'), Validators.required])],
+        mobileNumber: ['', Validators.compose([Validators.maxLength(10), Validators.minLength(10),Validators.pattern('[0-9]*'), Validators.required])],
+        emailAddress: ['', Validators.compose([Validators.minLength(2), Validators.pattern('[a-zA-Z ]*'), Validators.required])],
+        location: ['', Validators.compose([Validators.minLength(2), Validators.pattern('[a-zA-Z ]*'), Validators.required])],
+        category: ['', Validators.compose([Validators.required])],    
+    });
+    this.slideTwoForm = formBuilder.group({
+        timeEnd: ['', Validators.compose([Validators.required])],
+        dateEnd: ['', Validators.compose([Validators.required])],
+        dateStart: ['', Validators.compose([Validators.required])],
+        timeStart: ['', Validators.compose([Validators.required])],
+    }); 
+    this.slideFourForm = formBuilder.group({
+        postAsaBusiness: [false],
+        businessName: [''],
+        businessRegistrationNumber: [''],
+        businessWebsite: [''],
+    });
+     this.businessName = this.slideFourForm.controls['businessName'];   
+     this.businessWebsite = this.slideFourForm.controls['businessWebsite'];
+     this.businessRegistrationNumber = this.slideFourForm.controls['businessRegistrationNumber'];
+     this.slideThreeForm = formBuilder.group({
+        imageIsLoaded: [false],
+         imageRef: [""],
+    });
+  }
+
+ next(){
+        this.signupSlider.slideNext();
+    }
+ 
+    prev(){
+        this.signupSlider.slidePrev();
+    }
+    postAsaBusinessChanged(){
+      if(this.advert.postAsaBusiness){
+        this.businessName.setValidators([Validators.required]);
+          this.businessRegistrationNumber.setValidators([Validators.required]);
+           this.businessWebsite.setValidators([Validators.required]);
+            this.businessName.enable(false);
+          this.businessRegistrationNumber.enable(false);
+           this.businessWebsite.enable(false);
+      }
+      else{
+         this.businessName.disable(true);
+          this.businessRegistrationNumber.disable(true);
+           this.businessWebsite.disable(true);
+      }
+    }
+
+    getDateOnly(datetime){
+      var date;
+      var month = ("0" + (datetime.getMonth() + 1)).slice(-2);
+      var day = ("0" + (datetime.getDate())).slice(-2);
+      date = day + "/" + month + "/" + datetime.getFullYear();
+      return date;
+    }
+  save(){
+
+var startDate = this.getDateOnly(new Date(this.advert.dateStart));
+var endDate = this.getDateOnly(new Date(this.advert.dateEnd));
+ 
+    this.submitAttempt = true;
+ 
+    if(!this.slideOneForm.valid){
+        this.signupSlider.slideTo(0);
+    } 
+    else if(!this.slideTwoForm.valid){
+        this.signupSlider.slideTo(1);
+    }
+    else if(new Date(this.advert.dateStart) < new Date()){
+        let toast = this.toastCtrl.create({
+          message: 'Start date can not be in the past',
+          duration: 2000
+        });
+        toast.present();
+        this.signupSlider.slideTo(1);
+    }
+    else if(new Date(this.advert.dateEnd) < new Date()){
+       let toast = this.toastCtrl.create({
+          message: 'End date can not be in the past',
+          duration: 2000
+        });
+        toast.present();
+        this.signupSlider.slideTo(1);
+    }
+    else if(!this.slideThreeForm.valid){
+        this.signupSlider.slideTo(2);
+    }else if(!this.slideFourForm.valid){
+        this.signupSlider.slideTo(3);
+    }
+    else {
+      let confirm = this.alertCtrl.create({
+      title: 'DIsclamer',
+      message: 'Do you agree to use this lightsaber to do good across the intergalactic galaxy?',
+      buttons: [
+        {
+          text: 'Disagree',
+          handler: () => {
+          }
+        },
+        {
+          text: 'Agree',
+          handler: () => {
+           this.navCtrl.goToRoot("Billboard");
+            let toast = this.toastCtrl.create({
+                message: "Advert has been added successfully",
+                duration: 3000,
+                position: 'bottom'
+            });
+            toast.present();
+             
+          }
+        }
+      ]
+    });
+    confirm.present()
+    }
+  }
+
+    initializeItems() {
+    this.items = [
+      'Amsterdam',
+      'Bogota',
+    ];
+  }
+
+  getItems(ev: any) {
+    // Reset items back to all of the items
+    this.initializeItems();
+
+    // set val to the value of the searchbar
+    let val = ev.target.value;
+
+    // if the value is an empty string don't filter the items
+    if (val && val.trim() != '') {
+      this.items = this.items.filter((item) => {
+        return (item.toLowerCase().indexOf(val.toLowerCase()) > -1);
+      })
+    }
+ 
+}
+
+lastImage: string = null;
   loading: Loading;
   public presentActionSheet() {
     let toast = this.toastCtrl.create({
@@ -47,97 +227,7 @@ export class PostAnAdvertPage {
     actionSheet.present();
   }
 
-  public advert = {
-     name: undefined,
-      category: undefined,
-      dateStart: new Date().toISOString(), 
-      timeStart: new Date().getTime(), 
-      dateEnd:  new Date().toISOString(), 
-      timeEnd: new Date().getTime(), 
-      mobileNumber: undefined, 
-      emailAddress: undefined, 
-      location: undefined, 
-      postAsaBusiness: false, 
-      businessName: '', 
-      businessRegistrationNumber: '', 
-      businessWebsite: '', 
-      isApproved: false,
-      imageRef: undefined,
-      
-  };
-  advertForm: FormGroup;
-  adverts: FirebaseListObservable<any>;
- formIsSubmitted = false;
-  constructor(public navCtrl: NavController, public formBuilder: FormBuilder,  af: AngularFire, private camera: Camera, private transfer: Transfer, private file: File, private filePath: FilePath, public actionSheetCtrl: ActionSheetController, public toastCtrl: ToastController, public platform: Platform, public loadingCtrl: LoadingController) {
-      this.adverts = af.database.list('/advert');
-    this.advertForm = this.formBuilder.group({
-      name: ['', [Validators.required, Validators.minLength(2)]],
-      category: [undefined, Validators.required],
-      dateStart: ['', Validators.required], 
-      timeStart: ['', Validators.required], 
-      dateEnd: ['', Validators.required], 
-      timeEnd: ['', Validators.required], 
-      mobileNumber: ['', Validators.required], 
-      emailAddress: ['', Validators.required], 
-      location: ['', Validators.required], 
-      postAsaBusiness: [false, Validators.required], 
-      businessName: [''], 
-      businessRegistrationNumber: [''], 
-      businessWebsite: [''], 
-      isApproved: [false],
-      isFreeAdvert: [false],
-    });
-  }
-  
-  submit(): void {
-     
- 
-  // File name only
-  var filename = this.lastImage;
-      this.formIsSubmitted = false;
-       // File for Upload
-      var targetPath = this.pathForImage(this.lastImage);
-      if(this.advertForm.valid && this.lastImage != undefined){
-            let storageRef = firebase.storage().ref();
-    // Create a timestamp as filename
-    const filename = Math.floor(Date.now() / 1000);
-
-    // Create a reference to 'images/todays-date.jpg'
-    const imageRef = storageRef.child(`images/${filename}.jpg`);
-    imageRef.putString(targetPath, firebase.storage.StringFormat.DATA_URL).then((snapshot)=> {
-     // Do something here when the data is succesfully uploaded!
-     this.advert.imageRef = imageRef;
-       this.adverts.push(this.advert);
-
-        let toast = this.toastCtrl.create({
-          message: 'Advert is added successfully',
-          duration: 2000
-        });
-        toast.present();
-    });
-      
-      }
-  }
-
-isValid(field: string) {
-    let formField = this.advertForm.get(field);
-    return formField.valid || formField.pristine;
-  }
-
-  postAsaBusinessChanged() {
-        
-    if(!this.advert.postAsaBusiness){
-      this.advertForm.controls['businessName'].invalid;   
-      this.advertForm.controls['businessRegistrationNumber'].invalid;
-       this.advertForm.controls['businessWebsite'].invalid;
-    }else{
-       this.advertForm.controls['businessName'].valid;   
-      this.advertForm.controls['businessRegistrationNumber'].valid;
-       this.advertForm.controls['businessWebsite'].valid;
-    }
-  }
-
-public takePicture(sourceType) {
+  public takePicture(sourceType) {
   // Create options for the Camera Dialog
   var options = {
     quality: 100,
@@ -146,6 +236,15 @@ public takePicture(sourceType) {
     correctOrientation: true
   };
  
+//this.camera.getPicture({
+  //   sourceType: this.camera.PictureSourceType.SAVEDPHOTOALBUM,
+    // destinationType: this.camera.DestinationType.DATA_URL
+    //}).then((imageData) => {
+     // this.base64Image = 'data:image/jpeg;base64,'+imageData;
+     //}, (err) => {
+      //console.log(err);
+    //});
+
   // Get the data of an image
   this.camera.getPicture(options).then((imagePath) => {
     // Special handling for Android library
@@ -162,7 +261,7 @@ public takePicture(sourceType) {
       this.copyFileToLocalDir(correctPath, currentName, this.createFileName());
     }
   }, (err) => {
-    this.presentToast('Error while selecting image.');
+    this.presentToast('Error while selecting image.'+err);
   });
 }
 // Create a new name for the image
@@ -200,10 +299,4 @@ public pathForImage(img) {
   }
 }
 }
-
-//this.firebase.getToken()
-  //.then(token => console.log(`The token is ${token}`)) // save the token server-side and use it to push notifications to this device
-  //.catch(error => console.error('Error getting token', error));
-
-//this.firebase.onTokenRefresh()
- // .subscribe((token: string) => console.log(`Got a new token ${token}`));
+  
